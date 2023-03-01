e0ae26357ab7f4457b62efc82311927da4e25292
mindy <minzhou16@gmail.com>
2023-02-28 21:58:41 -0500

    Added send_email when throw exceptions.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- tests/send_grid_python/outbound_email_test.py
+++ tests/send_grid_python/outbound_email_test.py
@@ -1,8 +1,10 @@
 """This python module tests outbound_email module."""
+import pytest
+
 from send_grid_python.outbound_email import send_email_message
 
 
-def test_send_email_message(mocker, monkeypatch) -> None:
+def test_send_email_message_succeeds(mocker, monkeypatch) -> None:
     """Test send_email_message."""
     monkeypatch.setenv("to_emails", "test_to_emails")
     monkeypatch.setenv("from_email", "test_from_email")
@@ -39,3 +41,29 @@ def test_send_email_message(mocker, monkeypatch) -> None:
     }
     api_client_mock.assert_called_once_with(api_key="test_key")
     return_client_mock.send.assert_called_once_with(message=message_mock)
+
+
+def test_send_email_message_throws_error(mocker, caplog, monkeypatch) -> None:
+    """Test send_email_message when fails."""
+    monkeypatch.setenv("to_emails", "test_to_emails")
+    monkeypatch.setenv("from_email", "test_from_email")
+    monkeypatch.setenv("SENDGRID_API_KEY", "test_key")
+    monkeypatch.setenv("template_id", "test_template_id")
+    subject = "test_subject"
+    task = "test_task"
+    deadline = "test_deadline"
+    return_client_mock = mocker.Mock()
+    mocker.patch(
+        "send_grid_python.outbound_email.SendGridAPIClient",
+        return_value=return_client_mock,
+    )
+    return_client_mock.send.side_effect = Exception
+    expected_log = "There was an error sending email via SendGrid API"
+
+    with pytest.raises(Exception):
+        send_email_message(
+            subject=subject,
+            task=task,
+            deadline=deadline,
+        )
+    assert expected_log in caplog.text
