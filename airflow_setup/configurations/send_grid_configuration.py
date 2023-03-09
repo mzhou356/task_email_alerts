5b882bb15043afc37687010a51c2df5f6fdd8971
mindy <minzhou16@gmail.com>
2023-03-08 21:27:27 -0500

    add config for sendgrid dag.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- airflow_setup/configurations/send_grid_configuration.py
+++ airflow_setup/configurations/send_grid_configuration.py
@@ -1,1 +1,32 @@
 """This module contains send grid configuration."""
+from typing import List
+
+send_grid_task_schedule: List[str] = [
+    "00 00 25 5 *",
+    "00 00 25 7 *",
+    "00 00 25 9 *",
+]
+
+send_grid_dag_ids: List[str] = [
+    "couple_reminder_one",
+    "couple_reminder_two",
+    "couple_reminder_three",
+]
+
+send_grid_subjects: List[str] = [
+    "happy anniversary",
+    "happy birthday",
+    "happy birthday",
+]
+
+send_grid_tasks: List[str] = [
+    "buy gifts",
+    "buy gift",
+    "buy gift",
+]
+
+send_grid_deadlines: List[str] = [
+    "in a few days",
+    "in a few days",
+    "in a few days",
+]
