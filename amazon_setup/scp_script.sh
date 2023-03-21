780dab176e2ac0dd0725f21c211628ca620f1c71
mindy <minzhou16@gmail.com>
2023-03-20 21:02:29 -0400

    Added scp and ec2 bootstrap
    Signed-off-by: mindy <minzhou16@gmail.com>

--- amazon_setup/scp_script.sh
+++ amazon_setup/scp_script.sh
@@ -4,3 +4,4 @@
 
 scp -r "$HOME"/Documents/projects/task_email_alerts/send_grid_python ec2-user@scheduler.mindy-dossett.com:task_email_alerts/send_grid_python
 scp "$HOME"/Documents/projects/task_email_alerts/main.py ec2-user@scheduler.mindy-dossett.com:task_email_alerts/main.py
+scp "$HOME"/Documents/projects/task_email_alerts/requirements.txt ec2-user@scheduler.mindy-dossett.com:task_email_alerts/requirements.txt
