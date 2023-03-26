c1904e34198596b3ad3910a11a35b65b538964be
mindy <minzhou16@gmail.com>
2023-03-25 21:59:26 -0400

    renamed the ec2_instance script.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- amazon_setup/delete_running_ec2_instance_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-ec2_instance_id=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running \
---query 'Reservations[*].Instances[*].[InstanceId]' \
---output text)
-
-aws ec2 terminate-instances --instance-ids "$ec2_instance_id"
